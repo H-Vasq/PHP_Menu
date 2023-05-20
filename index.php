94048282572ac8981608e86d147e6ea0aedb4b1d
H-Vasq <123214691+H-Vasq@users.noreply.github.com>
2023-05-20 14:40:56 -0600

    Initial Layout

--- index.php
+++ index.php
@@ -5,8 +5,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/styles.css">
 </head>
-<body>
-    
+<body id="final-example">
+    <div class="wrapper">
+        <div id="banner">
+            <a href="/" title="Returns to Home">
+                <img src="Student/img/banner.png" alt="Frank Dining">
+            </a>
+        </div>
+        <div id="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="team.php">Team</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <div id="footer" class="cf">
+                <div class="column three"></div>
+                    <strong>Phone</strong>
+                    808.529.3819
+                <div class="column three"></div>
+                    <strong>Location</strong>
+                    123 Kapiolani Boulevart<br>
+                    Honolulu, HI
+                <div class="column three last">
+                    <strong>Hours</strong>
+                    <em>Tuesday - Thursday</em><br>
+                    1:00pm - 9:00pm <br><br>
+                    <em>Friday - Saturday</em><br>
+                    4:00pm - 11:00pm <br><br>
+                    <em>Sunday - Monday</em><br>
+                    Closed <br><br>
+                </div>
+            </div>
+            <small>&copy;2023 R V</small>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
