228f4f0ad5488cf8d1426a657768b737da465be1
H-Vasq <123214691+H-Vasq@users.noreply.github.com>
2023-05-23 14:19:01 -0600

    Templating start

---
+++ Student/includes/footer.php
@@ -0,0 +1,24 @@
+<div id="footer" class="cf">
+                <div class="column three"></div>
+                    <strong>Phone</strong>
+                    808.529.3819
+                <div class="column three"></div>
+                    <strong>Location</strong>
+                    123 Kapiolani Boulevart<br>
+                    Honolulu, HI
+                <div class="column three last">
+                    <strong>Hours</strong>
+                    <em>Tuesday - Thursday</em><br>
+                    1:00pm - 9:00pm <br><br>
+                    <em>Friday - Saturday</em><br>
+                    4:00pm - 11:00pm <br><br>
+                    <em>Sunday - Monday</em><br>
+                    Closed <br><br>
+                </div>
+            </div>
+            <small>&copy;2023 R V</small>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
