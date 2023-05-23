a523d2d9815ce65c2d46d7a8e5448791160ef0c2
H-Vasq <123214691+H-Vasq@users.noreply.github.com>
2023-05-23 14:56:54 -0600

    array edits

--- Student/includes/arrays.php
+++ Student/includes/arrays.php
@@ -17,4 +17,14 @@
             title   => "Contact"
         ),
     );
+
+    $menuItems = array(
+                    "club-sandwich" => array(
+                        title => "Club Sandwich",
+                        price => 11,
+                        blur => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ducimus recusandae eveniet reiciendis esse distinctio error unde saepe exercitationem, id at assumenda mollitia minima enim necessitatibus explicabo rem ab nesciunt dicta provident modi repellat! A, explicabo ab. Eligendi omnis tempore quidem architecto, labore quo commodi optio corporis consectetur nesciunt quia, ex alias delectus deserunt odit quae quasi modi! Ducimus, accusamus!"
+                        drink => "Coke"
+                    )
+
+    );
 ?>
